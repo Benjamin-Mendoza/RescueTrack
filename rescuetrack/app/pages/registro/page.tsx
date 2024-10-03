6a3d9198ee3767cbfbc3bfd8da0e58930d8b7cbd
TortugxdeuSa <beat.orellana@duocuc.cl>
2024-10-03 11:48:44 -0300

    Css Registro

--- rescuetrack/app/pages/registro/page.tsx
+++ rescuetrack/app/pages/registro/page.tsx
@@ -1,15 +1,43 @@
 import { Button } from "@nextui-org/button";
 
 const Registro = () => {
-
   return (
-    <div className="max-w-md mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Registro de Usuario</h1>
+    <div className="c1">
+    <div className="c2">
+      <h1 className="titulo">Registro de Usuario</h1>
       <form className="space-y-4">
-
+        
         <div>
           <label htmlFor="name" className="block text-sm font-medium">Nombre</label>
           <input type="text" name="nombre" id="nombre" className="mt-1 block w-full px-3 py-2 border rounded-md"/>
+        </div>
+
+        <div>
+          <label htmlFor="rol" className="block text-sm font-medium">Rol</label>
+          <select name="rol" id="rol" className="mt-1 block w-full px-3 py-2 border rounded-md">
+            <option value="">Seleccionar rol</option>
+            <option value="secretario">Secretario</option>
+            <option value="mecanico">Mecanico</option>
+            <option value="capitan">Capitan</option>
+            <option value="teniente">Teniente</option>
+          </select>
+        </div>
+
+        <div>
+          <label htmlFor="compania" className="block text-sm font-medium">Compañía</label>
+          <select name="compania" id="compania" className="mt-1 block w-full px-3 py-2 border rounded-md">
+            <option value="">Seleccionar compañía</option>
+            <option value="primera">Primera Compañía</option>
+            <option value="segunda">Segunda Compañía</option>
+            <option value="tercera">Tercera Compañía</option>
+            <option value="cuarta">Cuarta Compañía</option>
+            <option value="quinta">Quinta Compañía</option>
+            <option value="sexta">Sexta Compañía</option>
+            <option value="septima">Septima Compañía</option>
+            <option value="octava">Octava Compañía</option>
+            <option value="novena">Novena Compañía</option>
+            <option value="undecima">Undecima Compañía</option>
+          </select>
         </div>
 
         <div>
@@ -22,12 +50,10 @@ const Registro = () => {
           <input type="password" name="contrasena" id="contrasena" className="mt-1 block w-full px-3 py-2 border rounded-md"/>
         </div>
 
-        <Button type="submit" radius="full" color="primary">Registrarse</Button>
-        
+        <Button className="button" type="submit" radius="full" color="primary">Registrarse</Button>
       </form>
+    </div>
     </div>
   );
 };
 export default Registro;
-
-
